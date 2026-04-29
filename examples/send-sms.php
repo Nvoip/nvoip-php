91e80378202e4b5f1a1376521ef1f6062ae27977
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 18:18:17 -0300

    Expand PHP examples for auth, OTP, calls and WhatsApp

--- examples/send-sms.php
+++ examples/send-sms.php
@@ -6,14 +6,23 @@ require __DIR__ . '/../src/NvoipClient.php';
 
 use Nvoip\NvoipClient;
 
-$baseUrl = getenv('NVOIP_BASE_URL') ?: 'https://api.nvoip.com.br/v2';
-$numbersip = getenv('NVOIP_NUMBERSIP') ?: '';
-$userToken = getenv('NVOIP_USER_TOKEN') ?: '';
-$target = getenv('NVOIP_TARGET_NUMBER') ?: '11999999999';
-$message = getenv('NVOIP_SMS_MESSAGE') ?: 'Mensagem de teste Nvoip';
+$client = new NvoipClient(
+    getenv('NVOIP_BASE_URL') ?: 'https://api.nvoip.com.br/v2',
+    getenv('NVOIP_OAUTH_BASIC_AUTH') ?: null,
+    getenv('NVOIP_OAUTH_CLIENT_ID') ?: null,
+    getenv('NVOIP_OAUTH_CLIENT_SECRET') ?: null
+);
 
-$client = new NvoipClient($baseUrl);
-$oauth = $client->createAccessToken($numbersip, $userToken);
-$response = $client->sendSms($target, $message, false, $oauth['access_token'] ?? '');
+$oauth = $client->createAccessToken(
+    getenv('NVOIP_NUMBERSIP') ?: '',
+    getenv('NVOIP_USER_TOKEN') ?: ''
+);
+
+$response = $client->sendSms(
+    getenv('NVOIP_TARGET_NUMBER') ?: '11999999999',
+    getenv('NVOIP_SMS_MESSAGE') ?: 'Mensagem de teste Nvoip',
+    false,
+    $oauth['access_token'] ?? ''
+);
 
 echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE) . PHP_EOL;
