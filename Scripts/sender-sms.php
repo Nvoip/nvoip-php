12a1771f75c92991fc5850f4fc21c95100e2f3c4
Leandro Campos <40430440+leandronvoip@users.noreply.github.com>
2026-04-29 13:52:18 -0300

    feat: migrate PHP SMS example to API v2 OAuth

--- Scripts/sender-sms.php
+++ Scripts/sender-sms.php
@@ -1,62 +1,32 @@
-<!--
-Nvoip - Sender SMS PHP
+<?php
 
-Copyright (C) 2020 Nvoip Plataforma Telefonia Ltda
-Leandro Campos <https://www.linkedin.com/in/leandro-campos/>
-License https://www.gnu.org/licenses/gpl-3.0.html
+declare(strict_types=1);
 
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+require __DIR__ . '/../src/NvoipClient.php';
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+use Nvoip\NvoipClient;
 
-You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
--->
-<html>
- <head>
-  <title>SMS Trigger by Nvoip API</title>
- </head>
- <body>
- <?php
--------------------------------------------------------------------------------------
-  
-$ch = curl_init();
+$numbersip = $_GET['numbersip'] ?? getenv('NVOIP_NUMBERSIP') ?: '';
+$userToken = $_GET['user_token'] ?? getenv('NVOIP_USER_TOKEN') ?: '';
+$numberPhone = $_GET['numberPhone'] ?? $_GET['celular'] ?? '';
+$message = $_GET['message'] ?? $_GET['msg'] ?? '';
 
-curl_setopt($ch, CURLOPT_URL, "https://api.nvoip.com.br/v1/sms");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
+if ($numbersip === '' || $userToken === '' || $numberPhone === '' || $message === '') {
+    http_response_code(400);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(
+        [
+            'error' => 'Missing required parameters.',
+            'required' => ['numbersip', 'user_token', 'numberPhone', 'message'],
+        ],
+        JSON_UNESCAPED_UNICODE
+    );
+    exit;
+}
 
-curl_setopt($ch, CURLOPT_POST, TRUE);
+$client = new NvoipClient(getenv('NVOIP_BASE_URL') ?: 'https://api.nvoip.com.br/v2');
+$oauth = $client->createAccessToken($numbersip, $userToken);
+$response = $client->sendSms($numberPhone, $message, false, $oauth['access_token'] ?? '');
 
-#Token de autenticação. Você consegue este token criando sua conta gratuita em https://www.nvoip.com.br
-#Token Authentication. You can get this token by creating your free account at https://www.nvoip.com.br
-$token_auth = $_GET["token_auth"];
-
-#Número de celular que irá receber o SMS (No momento, só é possível enviar SMS para números Brasileiros). Não colocar DDI, apenas DDD + número. Exemplo: 11911112222.
-#Mobile number that will receive SMS (Currently, it is only possible to send SMS to Brazilian numbers). Do not put DDI, just DDD + number. Example: 11911112222.
-$celular = $_GET["celular"];
-
-#Mensagem a ser enviada. Lembre-se de não utilizar acentos ou caracteres especiais e limite-se a 140 caracteres.
-#Message to send. Remember not to use accents or special characters and limit yourself to 140 characters.
-$msg = $_GET["msg"];
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, "{
-  \"celular\": \"$celular\",
-  \"msg\": \"$msg\"
-}");
-
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  "Content-Type: application/json",
-  "token_auth: $token_auth"
-));
-
-$response = curl_exec($ch);
-curl_close($ch);
-
-var_dump($response);
-
-
-?>
- </body>
-</html>
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
